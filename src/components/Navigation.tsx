705ff915324a6645ccb5213f6958c144acf9bbcc
otmane Maarir <53414096+OtmaneM@users.noreply.github.com>
2025-09-22 12:01:55 +0100

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -7,7 +7,7 @@ const Navigation = () => {
       <div className="container mx-auto px-6 py-4">
         <div className="flex items-center justify-between">
           <div className="text-2xl font-bold text-primary">
-            <img src={logo} alt="Logo" className="h-8" />
+            <img src={logo} alt="Logo" className= width: 4px; height: 4px;/>
           </div>
           
           <div className="hidden md:flex items-center space-x-8">
